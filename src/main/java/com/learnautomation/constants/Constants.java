78ddbd3fcc876e9b014db785e31625138130661b
RatnaHarika001 <harikaqueen2986@gmail.com>
2023-06-21 15:49:11 +0530

    files addition

--- src/main/java/com/learnautomation/constants/Constants.java
+++ src/main/java/com/learnautomation/constants/Constants.java
@@ -3,7 +3,7 @@ package com.learnautomation.constants;
 public interface Constants 
 {
 	
-	public String userDoesNotExist = "USEr Email Doesn't Exist";
+	public String userDoesNotExist = "USER Email Doesn't Exist";
 	public String emptyPassword = "Password is required";
 	public String userPassMisMatch = "Email and Password Doesn't match";
 
