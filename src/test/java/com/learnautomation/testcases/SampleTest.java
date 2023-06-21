3010376367c9abb298da2f84e58d876c607e4f4d
RatnaHarika001 <harikaqueen2986@gmail.com>
2023-06-21 09:56:49 +0530

    new test added in sample test class

--- src/test/java/com/learnautomation/testcases/SampleTest.java
+++ src/test/java/com/learnautomation/testcases/SampleTest.java
@@ -1,7 +1,15 @@
 package com.learnautomation.testcases;
 
+import org.testng.annotations.Test;
+
 public class SampleTest {
 
 	
 //	addsample
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Sample test for github");
+	}
 }
