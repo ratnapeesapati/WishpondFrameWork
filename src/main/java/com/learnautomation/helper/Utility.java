42ccb32f125e07ae001c3dd14b444eacbd7fe297
ratnapeesapatie <ratna.peesapati@wishpond.com>
2023-08-16 18:50:03 +0530

    changes to POM file

--- src/main/java/com/learnautomation/helper/Utility.java
+++ src/main/java/com/learnautomation/helper/Utility.java
@@ -18,7 +18,6 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.devtools.v109.io.IO;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.io.FileHandler;
